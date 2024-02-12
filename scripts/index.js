da5f70f4a1e9e6c4626d5a55a837dbd4112da654
Anna Vasileva <vasilieva.a@icloud.com>
2024-02-12 13:36:59 -0500

    sprint 4 stage 1

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+const initialCards = [
+  {
+    name: "Zion Park",
+    link: "https://unsplash.com/photos/brown-and-gray-rocky-mountain-under-white-cloudy-sky-during-daytime-G6jvH_hI5Pc",
+  },
+  {
+    name: "Yellowstone",
+    link: "https://unsplash.com/photos/cattle-on-field-D1t6I4mjd8E",
+  },
+  {
+    name: "Caples Lake",
+    link: "https://unsplash.com/photos/snow-covered-mountain-near-lake-during-daytime-8vzh-Hyp8_0",
+  },
+  {
+    name: "Glacier National Park",
+    link: "https://unsplash.com/photos/green-tree-on-white-rock-near-body-of-water-during-daytime-RE95MskGbaQ",
+  },
+  {
+    name: "Point Reyes",
+    link: "https://unsplash.com/photos/landscape-photography-of-sea-waves-fltwvwbApwA",
+  },
+  {
+    name: "Tibble Fork Reservoir",
+    link: "https://unsplash.com/photos/snow-covered-tree-near-body-of-water-r6TLRDY4Ll0",
+  },
+];
+
