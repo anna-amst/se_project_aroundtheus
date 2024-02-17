8f189525b1b3fd31026b30e131f1c5d8732b80b8
Anna Vasileva <vasilieva.a@icloud.com>
2024-02-17 01:17:18 -0500

    stage4 sprint4

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,75 @@
 const initialCards = [
   {
     name: "Zion Park",
-    link: "https://unsplash.com/photos/brown-and-gray-rocky-mountain-under-white-cloudy-sky-during-daytime-G6jvH_hI5Pc",
+    link: "https://images.unsplash.com/photo-1587141105209-2a7ee89bce02?q=80&w=2987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Yellowstone",
-    link: "https://unsplash.com/photos/cattle-on-field-D1t6I4mjd8E",
+    link: "https://images.unsplash.com/photo-1581517175451-4d5d810582dc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "Caples Lake",
-    link: "https://unsplash.com/photos/snow-covered-mountain-near-lake-during-daytime-8vzh-Hyp8_0",
+    name: "Pacific Grove",
+    link: "https://images.unsplash.com/photo-1682094245632-b0fc877bd8ae?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Glacier National Park",
-    link: "https://unsplash.com/photos/green-tree-on-white-rock-near-body-of-water-during-daytime-RE95MskGbaQ",
+    link: "https://images.unsplash.com/photo-1610944092619-c88625f3348f?q=80&w=2148&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Point Reyes",
-    link: "https://unsplash.com/photos/landscape-photography-of-sea-waves-fltwvwbApwA",
+    link: "https://images.unsplash.com/photo-1529380791549-da5e3bf4d91d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Tibble Fork Reservoir",
-    link: "https://unsplash.com/photos/snow-covered-tree-near-body-of-water-r6TLRDY4Ll0",
+    link: "https://images.unsplash.com/photo-1455156218388-5e61b526818b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
 const editButton = document.querySelector(".profile__edit-button");
 const editModal = document.querySelector(".modal");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const inputTitle = document.querySelector(".modal__input-title");
+const inputSubtitle = document.querySelector(".modal__input-subtitle");
+const closeButton = document.querySelector(".modal__close-button");
+const profileEditForm = editModal.querySelector(".modal__container");
+const cardList = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = inputTitle.value;
+  profileSubtitle.textContent = inputSubtitle.value;
+  closeModal();
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  cardTitleElement.textContent = cardData.name;
+  cardImageElement.src = cardData.link;
+  cardImageElement.alt = cardData.name;
+  return cardElement;
+}
 
 editButton.addEventListener("click", () => {
+  inputTitle.value = profileTitle.textContent;
+  inputSubtitle.value = profileSubtitle.textContent;
   editModal.classList.add("modal_opened");
 });
 
-const closeButton = document.querySelector(".modal__close-button");
-
 closeButton.addEventListener("click", () => {
-  editModal.classList.remove("modal_opened");
+  closeModal();
+});
+
+profileEditForm.addEventListener("submit", handleProfileFormSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
 });
